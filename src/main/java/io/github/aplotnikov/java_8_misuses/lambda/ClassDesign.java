b882572a9b4ad5d7511cc08c564f08b599448b0d
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2018-12-29 23:51:21 +0100

    Refactor code

--- src/main/java/io/github/aplotnikov/java_8_misuses/lambda/ClassDesign.java
+++ src/main/java/io/github/aplotnikov/java_8_misuses/lambda/ClassDesign.java
@@ -1,7 +1,9 @@
 package io.github.aplotnikov.java_8_misuses.lambda;
 
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 import static io.github.aplotnikov.java_8_misuses.utils.Annotations.Bad;
 import static io.github.aplotnikov.java_8_misuses.utils.Annotations.Good;
@@ -15,12 +17,19 @@ class ClassDesign {
             T process(Callable<T> function);
 
             T process(Runnable function);
+
+            T process(UnaryOperator<T> function);
+
+            <R> R process(Function<T, R> function);
         }
 
         void usage(AmbiguousService<String> service, Supplier<String> supplier) {
             //which method you intended to call??? both are acceptable.
-            service.process(() -> { supplier.get(); });
+            service.process(() -> {
+                supplier.get();
+            });
             service.process(() -> supplier.get());
+            service.process(String::toUpperCase);
         }
     }
 
@@ -31,10 +40,15 @@ class ClassDesign {
             <R> R processAsync(Callable<T> function);
 
             T process(Runnable function);
+
+            T map(UnaryOperator<T> function);
+
+            <R> R convert(Function<T, R> function);
         }
 
         void usage(ClearService<String> service, Supplier<String> supplier) {
             service.process(supplier::get);
+            service.map(String::toUpperCase);
         }
     }
 }
