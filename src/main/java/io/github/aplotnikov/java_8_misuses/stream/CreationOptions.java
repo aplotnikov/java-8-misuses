0f79adb82760fd5454f84e351b20fe99885b8eca
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2018-12-30 00:35:11 +0100

    Remove unneeded code

--- src/main/java/io/github/aplotnikov/java_8_misuses/stream/CreationOptions.java
+++ src/main/java/io/github/aplotnikov/java_8_misuses/stream/CreationOptions.java
@@ -18,51 +18,51 @@ import static io.github.aplotnikov.java_8_misuses.utils.Annotations.Ugly;
 class CreationOptions {
 
     @Ugly
-    public Stream<Type> getStreamOfApplicationTypesFromArrayAsList() {
+    Stream<Type> getStreamOfApplicationTypesFromArrayAsList() {
         return Arrays.asList(MAIN, EXTENSION).stream();
     }
 
     @Ugly
-    public Stream<Type> getStreamOfApplicationTypesForFactoryMethod() {
+    Stream<Type> getStreamOfApplicationTypesForFactoryMethod() {
         return List.of(MAIN, EXTENSION).stream();
     }
 
     @Good
-    public Stream<Type> getStreamFromElements() {
+    Stream<Type> getStreamFromElements() {
         return Stream.of(MAIN, EXTENSION);
     }
 
     @Ugly
-    public Stream<Loan> generateStreamByMappingCopies(int n) {
+    Stream<Loan> generateStreamByMappingCopies(int n) {
         return Collections.nCopies(n, "ignored")
                           .stream()
                           .map(ignored -> new Loan());
     }
 
     @Ugly
-    public Stream<Loan> generateStreamFromRange(int n) {
+    Stream<Loan> generateStreamFromRange(int n) {
         return IntStream.range(0, n).mapToObj(ignored -> new Loan());
     }
 
     @Good
-    public Stream<Loan> generateStreamFromSupplierWithLimit(int n) {
+    Stream<Loan> generateStreamFromSupplierWithLimit(int n) {
         return Stream.generate(Loan::new).limit(n);
     }
 
     @Ugly
-    public Stream<Loan> generateStreamFromArrayWithRange(Loan[] loans, int max) {
+    Stream<Loan> generateStreamFromArrayWithRange(Loan[] loans, int max) {
         int to = Integer.min(loans.length, max);
         return IntStream.range(0, to).mapToObj(i -> loans[i]);
     }
 
     @Bad
-    public Stream<Loan> generateStreamFromArrayStreamWithRange(Loan[] loans, int max) {
+    Stream<Loan> generateStreamFromArrayStreamWithRange(Loan[] loans, int max) {
         int to = Integer.min(loans.length, max);
         return Arrays.stream(loans, 0, to);
     }
 
     @Good
-    public Stream<Loan> generateStreamFromArrayWithLimit(Loan[] loans, int max) {
+    Stream<Loan> generateStreamFromArrayWithLimit(Loan[] loans, int max) {
         return Stream.of(loans).limit(max);
     }
 }
